7d45111a9981b0d3eea57f4732dde3dbf90d6162
Saif Ur Rehman <saif@paralogic.io>
2024-11-06 15:46:58 +0500

    chore: moved files to automotive app

--- erpnext/vehicles/report/vehicle_service_summary/vehicle_service_summary.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
-from erpnext.projects.report.project_sales_summary.project_sales_summary import ProjectSalesSummaryReport
-
-
-def execute(filters=None):
-	return ProjectSalesSummaryReport(filters, is_vehicle_service=True).run()
