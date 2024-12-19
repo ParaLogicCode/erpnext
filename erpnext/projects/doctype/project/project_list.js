4ee18d417cb559f32dc6d048aa407c75561e0a43
Saif Ur Rehman <saif@paralogic.io>
2024-12-20 01:44:41 +0500

    chore(Project): do not show percentage

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -1,23 +1,18 @@
 frappe.listview_settings['Project'] = {
 	add_fields: ["project_status", "status", "indicator_color", "priority", "is_active", "percent_complete", "project_name"],
 	get_indicator: function(doc) {
-		var percentage = "";
-		if (doc.percent_complete && doc.status != "Completed") {
-			percentage = " " + __("({0}%)", [cint(doc.percent_complete)]);
-		}
-
-		var color_map = {
+		let color_map = {
 			"Open": "orange",
 			"Completed": "green",
 			"Closed": "green",
 			"Cancelled": "light-gray",
 		}
 
-		var guessed_color = color_map[doc.status] || frappe.utils.guess_colour(doc.status);
+		let guessed_color = color_map[doc.status] || frappe.utils.guess_colour(doc.status);
 
 		if (doc.project_status) {
 			return [
-				__(doc.project_status) + percentage,
+				__(doc.project_status),
 				doc.indicator_color || guessed_color,
 				"project_status,=," + doc.project_status
 			];
