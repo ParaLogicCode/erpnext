ffc5ac4182a607590a0c49f7d1a3a9fe34c6bb58
Saif Ur Rehman <saif@paralogic.io>
2025-01-21 00:31:57 +0500

    fix: cleanup

--- erpnext/public/js/utils/insurance_surveyor_quick_entry.js
+++ erpnext/public/js/utils/insurance_surveyor_quick_entry.js
@@ -1,10 +1,6 @@
 frappe.provide('frappe.ui.form');
 
 frappe.ui.form.InsuranceSurveyorQuickEntryForm = class InsuranceSurveyorQuickEntryForm extends frappe.ui.form.QuickEntryForm {
-	init(doctype, after_insert) {
-		super.init(doctype, after_insert);
-	}
-
 	render_dialog() {
 		super.render_dialog();
 		if (this.dialog.get_field("insurance_company")) {
